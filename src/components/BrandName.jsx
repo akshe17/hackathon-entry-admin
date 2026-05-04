f7ceaaecf7ed4c6ccff14957ea858b6cfcb875d6
vladimer <vladimer177@yahoo.com>
2026-05-04 19:34:56 +0800

    updated brandname with logo icon

--- src/components/BrandName.jsx
+++ src/components/BrandName.jsx
@@ -1,18 +1,22 @@
+import KainWiseLogo from "../../public/images/KainWiseLogo.svg";
 export function BrandName({ size = "1em", color }) {
   return (
-    <span style={{ color }}>
-      Kain
-      <span
-        style={{
-          fontFamily: "'Great Vibes', cursive",
-          fontWeight: 400,
-          fontSize: `calc(${size} * 1.4)`,
-          marginLeft: "-0.15em",
-          verticalAlign: "middle",
-        }}
-      >
-        Wise
+    <div className="flex flex-center  items-center justify-center flex-row">
+      <img src={KainWiseLogo} height={50} width={50} alt="KainWise Logo" />
+      <span style={{ color }}>
+        Kain
+        <span
+          style={{
+            fontFamily: "'Great Vibes', cursive",
+            fontWeight: 400,
+            fontSize: `calc(${size} * 1.4)`,
+            marginLeft: "-0.15em",
+            verticalAlign: "middle",
+          }}
+        >
+          Wise
+        </span>
       </span>
-    </span>
+    </div>
   );
 }
